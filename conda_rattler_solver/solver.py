bf40c5ea145a65f4d10cc7523c94f635963fc465
Travis Hathaway <travis.j.hathaway@gmail.com>
2026-09-23 14:26:56 +0200

    Add doc string for `_maybe_raise_for_problems` (#153)
    * Add doc string for `_maybe_raise_for_problems`
    
    This adds a doc string to the `_maybe_raise_for_problems` to give a little more context around why we parsing the error string for a MatchSpec. It also links to a feature request that would make this process a little more robust and this function no loner necessary.
    
    * Apply batched suggestions from code review
    
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- conda_rattler_solver/solver.py
+++ conda_rattler_solver/solver.py
@@ -661,6 +661,19 @@ class RattlerSolver(Solver):
     def _maybe_raise_for_problems(
         self, problems: str, in_state: SolverInputState, out_state: SolverOutputState
     ):
+        """
+        Parse `problems` for suitable MatchSpecs and then mutate out_state to add more
+        context for an error report. Two exceptions may be raised:
+        - PackagesNotFoundError: One or more packages are missing in the channel
+        - RattlerUnsatisfiableError: A solver conflict was found
+        
+        When called from conda-build, the parsed specs are dispatched to
+        `._maybe_raise_for_conda_build()`.
+
+        TODO: This is temporary work around until we can receive more structured output from
+              rattler solver that includes MatchSpec definitions.
+              See: https://github.com/conda/rattler/issues/1961
+        """
         unsatisfiable = {}
         not_found = {}
         for line in problems.splitlines():
